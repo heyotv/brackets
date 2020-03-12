3759870c320164db7fc6c2c56305fa467f9af671
heyotv <hey.tumilovich@gmail.com>
2020-03-12 15:04:45 +0300

    feat: add soluton

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let newConfig = bracketsConfig.map(el => el.join(""));
+
+  for (let i = 0; i <= newConfig.length; i++) {
+      if (str.includes(newConfig[i])) {
+          str = str.replace(newConfig[i], "");
+          console.log(i);
+          i -= newConfig.length;
+      }
+  }
+  return str ? false : true;
 }
